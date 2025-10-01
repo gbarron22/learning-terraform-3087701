a6ffb908b4405030012aa44212de55fc4d378cbd
gbarron22 <117693409+gbarron22@users.noreply.github.com>
2025-10-01 13:18:09 -0400

    load balancer

--- main.tf
+++ main.tf
@@ -45,6 +45,40 @@ resource "aws_instance" "web" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name            = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = modeule.blog_sg.security_group_id
+
+http_tcp_listeners = [
+  {
+    port               = 80
+    protocol           = "HTTP"
+    target_group_index = 0
+  }
+]
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instnace.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
